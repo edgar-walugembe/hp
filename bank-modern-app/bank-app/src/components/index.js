6f0926e298a5b179555b191786225eab8ef28073
windows <edgar.walugembe.upti@gmail.com>
2023-11-20 09:52:28 +0300

    hp 0005

---
+++ bank-modern-app/bank-app/src/components/index.js
@@ -0,0 +1,23 @@
+import Navbar from "./Navbar";
+import Hero from "./Hero";
+import Billing from "./Billing";
+import Business from "./Business";
+import CardDeal from "./CardDeal";
+import Clients from "./Clients";
+import Testimonials from "./Testimonials";
+import Footer from "./Footer";
+import CTA from "./CTA";
+import Stats from "./Stats";
+
+export {
+  Navbar,
+  Hero,
+  Billing,
+  Business,
+  CardDeal,
+  Clients,
+  Testimonials,
+  Footer,
+  CTA,
+  Stats,
+};
