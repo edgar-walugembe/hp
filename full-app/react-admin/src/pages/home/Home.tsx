9e084ea97e5895d7f85b1c7d769bd6af8add483f
windows <edgar.walugembe.upti@gmail.com>
2023-11-27 18:34:36 +0300

    hp 0005

--- full-app/react-admin/src/pages/home/Home.tsx
+++ full-app/react-admin/src/pages/home/Home.tsx
@@ -1,3 +1,4 @@
+import ChartBox from "../../components/chartBox/ChartBox";
 import TopBox from "../../components/topBox/topBox";
 import "./home.scss";
 
@@ -7,10 +8,18 @@ const Home = () => {
       <div className="box box1">
         <TopBox />
       </div>
-      <div className="box box2">Box2</div>
-      <div className="box box3">Box3</div>
-      <div className="box box4">Box4</div>
-      <div className="box box5">Box5</div>
+      <div className="box box2">
+        <ChartBox />
+      </div>
+      <div className="box box3">
+        <ChartBox />
+      </div>
+      <div className="box box4">
+        <ChartBox />
+      </div>
+      <div className="box box5">
+        <ChartBox />
+      </div>
       <div className="box box6">Box6</div>
       <div className="box box7">Box7</div>
       <div className="box box8">Box8</div>
