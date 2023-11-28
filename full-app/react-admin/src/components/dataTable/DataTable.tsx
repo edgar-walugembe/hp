94034b57aa153f664841713913f9392e256b12ed
windows <edgar.walugembe.upti@gmail.com>
2023-11-28 18:05:20 +0300

    hp 0006

---
+++ full-app/react-admin/src/components/dataTable/DataTable.tsx
@@ -0,0 +1,9 @@
+import "./dataTable.scss";
+
+const DataTable = () => {
+  return <div className="dataTable">DataTable</div>;
+};
+
+export default DataTable;
+
+//1:24:04
--- full-app/react-admin/src/pages/users/Users.tsx
+++ full-app/react-admin/src/pages/users/Users.tsx
@@ -1,7 +1,14 @@
 import "./users.scss";
 
 const Users = () => {
-  return <div className="users">Users</div>;
+  return (
+    <div className="users">
+      <div className="info">
+        <h1>Users</h1>
+        <button>Add New User</button>
+      </div>
+    </div>
+  );
 };
 
 export default Users;
