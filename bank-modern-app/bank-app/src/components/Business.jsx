05ab80c56b0b3670eb4fd5921a2d90c231fc17f9
windows <edgar.walugembe.upti@gmail.com>
2023-12-11 14:43:19 +0300

    hp 0007

--- bank-modern-app/bank-app/src/components/Business.jsx
+++ bank-modern-app/bank-app/src/components/Business.jsx
@@ -6,11 +6,17 @@ import Button from "./Button";
 const FeatureCard = ({ icon, title, content, index }) => (
   <div
     className={`flex flex-row p-6 rounded-[20px] ${
-      index == !features.length - 1
-    } ? "mb-6" : "mb-0" `}
+      index == !features.length - 1 ? "mb-6" : "mb-0"
+    } feature-card`}
   >
-    <div>
+    <div
+      className={`w-[64px] h-[64px] rounded-full ${styles.flexCenter} bg-dimBlue`}
+    >
       <img src={icon} alt={title} className="w-[50%] h-[50%] object-contain" />
+    </div>
+    <div className="flex-1 flex flex-col ml-3">
+      <h4>{title}</h4>
+      <p>{content}</p>
     </div>
   </div>
 );
