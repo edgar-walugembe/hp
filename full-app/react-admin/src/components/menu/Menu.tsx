f2b00421e5605c4c462819957ffa202c27eb97ba
windows <edgar.walugembe.upti@gmail.com>
2023-12-14 17:00:09 +0300

    hp 0007

--- full-app/react-admin/src/components/menu/Menu.tsx
+++ full-app/react-admin/src/components/menu/Menu.tsx
@@ -11,7 +11,7 @@ const Menu = () => {
           {item.listItems.map((listItem) => (
             <Link to={listItem.url} className="listItem" key={listItem.id}>
               <img src={listItem.icon} alt={listItem.title} />
-              <span className="ListItemTitle">{listItem.title}</span>
+              <span className="listItemTitle">{listItem.title}</span>
             </Link>
           ))}
         </div>
